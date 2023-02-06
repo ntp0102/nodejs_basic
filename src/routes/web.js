1e854b2bcf2a1ccd749e61332aceaf3e9e179f7c
ntp0102 <phuc.nguyenthien2000@hcmut.edu.vn>
2023-02-06 16:02:25 +0700

    3. MVC and thiet ke code theo chuan phan1

---
+++ src/routes/web.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import homeController from '../controller/homeController'
+
+let router = express.Router();
+
+const initWebRoute = (app) => {
+    router.get('/', homeController.getHomepage);
+    
+    router.get('/about', (req,res) => {
+        res.send(`I'm NTP`)
+    })
+
+    return app.use('/', router)   //tiền tố mặc định của website, ở đây là không dùng gì cả, vidu /api/version
+}
+
+module.exports = initWebRoute;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
 require('dotenv').config(); //khai bao file .env
+import initWebroute from './routes/web'
 
 const app = express();
 const port = process.env.PORT || 8080; // truong hop ko doc dc .env se chon 8080
 
+//setup viewEngine
 configViewEngine(app);
+//init web route
+initWebroute(app);
 
-app.get('/', (req,res) => {
-    res.render('index.ejs')
-})
-
-app.get('/about', (req,res) => {
-    res.send(`I'm NTP`)
-})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
