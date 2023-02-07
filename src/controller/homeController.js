6be19fb282cb7120537dda3fc595da600f059bf4
ntp0102 <phuc.nguyenthien2000@hcmut.edu.vn>
2023-02-07 14:15:16 +0700

    5. create navbar, ejsloop and render theo id

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,20 +1,16 @@
-import connection from "../configs/connectDB"
+import connection from "../configs/connectDB";
 
+let getHomepage = (req, res) => {
+  //logic
+  connection.query("SELECT * FROM `users` ", function (err, results, fields) {
+    //   console.log('>>> Check mysql');
+    //   console.log(results); // results contains rows returned by server
+    //   console.log(results[0]); // results contains rows returned by server
 
-let getHomepage = (req,res) => {
-    //logic
-    connection.query(
-        'SELECT * FROM `users` ',
-        function(err, results, fields) {
-          console.log('>>> Check mysql');
-          console.log(results); // results contains rows returned by server
-          console.log(results[0]); // results contains rows returned by server
-          
-          return res.render('index.ejs', {dataUser: JSON.stringify(results) })
-        }
-      );
-}
+    return res.render("index.ejs", { dataUser: results });
+  });
+};
 
 module.exports = {
-    getHomepage
-}
+  getHomepage,
+};
